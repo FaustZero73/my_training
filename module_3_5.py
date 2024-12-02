85d0f8a0564c1949998d3121dddc42ff4a64fb6d
FaustZero73 <kulakova612@gmail.com>
2024-12-02 21:52:09 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,16 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    if str_number.endswith('0'):  # убирать нули
+        str_number = str_number[:len(str_number) - 1]
     if len(str_number) > 1:
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
 
+
 result = get_multiplied_digits(40203)
 print(result)
 
+result2 = get_multiplied_digits(402030)
+print(result2)
