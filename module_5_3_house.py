813dc6f61345b0fc975cd5dc778ad1b2cae374ea
FaustZero73 <kulakova612@gmail.com>
2024-12-04 00:20:13 +0300

    Add files via upload

--- module_5_3_house.py
+++ module_5_3_house.py
@@ -10,8 +10,7 @@ class House:
         return name_gk
 
     def __eq__(self, other): #должен возвращать True, если количество этажей одинаковое у self и у other
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
+        return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other): #должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
@@ -45,8 +44,6 @@ class House:
         if isinstance(value, int):
             self.number_of_floors += value
         return self
-
-#Остальные методы арифметических операторов, где self - x, other - y:
 
 
 h1 = House('ЖК Эльбрус', 10)
